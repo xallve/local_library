d0008861b8fb4368c88f57ed3776e3425c7d51c0
Bogdan <prodanov1540@gmail.com>
2021-10-31 18:15:00 +0200

    Added models

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
 
 class Genre(models.Model):
@@ -44,3 +45,59 @@ class Book(models.Model):
         Returns the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    """
+    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+
+    imprint = models.CharField(max_length=200)
+
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+
+    def __str(self):
+        """
+        String for representing the Model object
+        """
+        return '{0}({1})'.format(self.id, self.book.title)
+
+
+class Author(models.Model):
+    """
+    Model representing an author.
+    """
+    first_name = models.CharField(max_length=100)
+
+    last_name = models.CharField(max_length=100)
+
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular author instance.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return '{0}, {1}'.format(self.last_name, self.first_name)
