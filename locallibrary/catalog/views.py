65d45e07eda734b0eda964ba3ab6caef4ed7c812
Bogdan <prodanov1540@gmail.com>
2022-02-16 23:33:05 +0200

    ---

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -35,11 +35,15 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
     return render(
         request,
         'index.html',
         context={'num_books': num_books,
                  'num_instances': num_instances,
                  'num_instances_available': num_instances_available,
-                 'num_authors': num_authors},
+                 'num_authors': num_authors,
+                 'num_visits': num_visits},
     )
